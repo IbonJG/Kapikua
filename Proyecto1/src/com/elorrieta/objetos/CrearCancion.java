ecb377b37eeac2bb9eaa9af4ed0c84fd21061220
cursos_externos <cursos_externos@HZ327416.elorrieta.int>
2021-04-20 12:31:22 +0200

    a ver si si o si no

---
+++ Proyecto1/src/com/elorrieta/objetos/CrearCancion.java
@@ -0,0 +1,13 @@
+package com.elorrieta.objetos;
+
+import java.util.Scanner;
+
+public class CrearCancion {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		teclado.close();
+	}
+
+}
